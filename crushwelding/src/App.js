8cd628781efc2bc85fda9ed94efe0537fe48c333
jiovanie <jiovaniemartinez@gmail.com>
2021-05-10 20:44:23 -0500

    working on the navbar

--- crushwelding/src/App.js
+++ crushwelding/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+   <div>
+     <Navbar/>
+   </div>
   );
 }
 
---
+++ crushwelding/src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+
+function Navbar () { 
+    return (
+    <div className= "navbar-parent">
+        <ul>
+            <li>
+                Contact Us
+            </li>
+            <li>
+                About Crush Welding
+            </li>
+        </ul>
+    </div>)
+}
+
+export default Navbar;
